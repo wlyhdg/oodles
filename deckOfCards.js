732dc5077b40b9211984b8910fb8f789f7f53ada
wlyhdg <waelwc3@gmail.com>
2020-07-28 20:07:43 -0700

    Create deckOfCards.js

---
+++ deckOfCards.js
@@ -0,0 +1,29 @@
+/*
+Deck
+- Array of 52 cards
+- shuffle method
+- include jokers method
+- giveCard method (decrement from deck)
+
+Card
+- number
+- color
+- suit
+
+Person
+- array or cards <Hand>
+- get sum of cards
+- getAceChoice method
+
+AbstractValue10Class
+- contains all 10, J, Q, K
+- number is 10
+
+Pot
+- all the throw away cards
+- have limit on pot quantity -> if exceeds threshold put into deck, then shuffle
+
+special cases: 
+_ Ace being 1 or 11
+_ Joker being included
+*/
